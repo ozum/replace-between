0ae6a4eb103706bae47e95157f0cf1e820d10a14
Ozum <ozum@ozum.net>
2017-10-07 10:56:10 +0300

    Switched from fs-promise to fs-extra

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path      = require('path');
 const chalk     = require('chalk');
-const fs        = require('fs-promise');
+const fs        = require('fs-extra');
 const getStdin  = require('get-stdin');
 
 const commentTypes = {
